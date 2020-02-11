d4934bd95ec986a7a007411a627df9400d3c425b
Patricia Ehrhardt <pehrhardt@epublishing.com>
2020-02-10 21:39:18 -0800

    DRY up controller methods.

--- app/controllers/clues_controller.rb
+++ app/controllers/clues_controller.rb
@@ -3,12 +3,19 @@ class CluesController < ApplicationController
   require 'json'
 
   def index
-    @clues = get_response
+    @clues_count = get_clues.count
+    @clues = get_clues
   end
 
-  def get_response
+  def get_clues
+    get_response("clues/?category=25")
+    .select{ |c| c["airdate"] >= "1996" && c["value"].present?}
+    .sort_by{ |d| d["value"] }
+  end
+
+  def get_response(url)
     base_url = "http://jservice.io/api/"
-    response = RestClient.get(base_url + "clues/?category=25" )
+    response = RestClient.get(base_url + url)
     JSON.parse(response)
   end
 end
